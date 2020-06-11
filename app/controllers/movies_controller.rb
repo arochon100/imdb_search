2e3d0b1a53e67444687aabb9dcc76c96f2b504a1
antoine rochon <antoine.rochon@hello-houston.com>
2020-06-11 16:38:27 +0200

    search

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,11 @@
+class MoviesController < ApplicationController
+
+
+    def index
+        if params[:query].present?
+        @movies = Movie.where("title ILIKE ?", "%#{params[:query]}%")
+        else
+        @movies = Movie.all
+        end
+    end
+end
